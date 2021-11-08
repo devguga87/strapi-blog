81ba14892c26e4592e29fd633fedec3536af1a0d
Gustavo Seabra <devguga87@gmail.com>
2021-11-08 12:19:11 -0300

    feature:strapi blog first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '67fe97dcc51689b5a5f710c471739d79'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7e579682-2fb0-4466-a25c-0d931c947aa0'
+};
